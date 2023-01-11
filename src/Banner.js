89c6d3ab3b4924bf1076faa8d42d9a1ff30cd26b
vishal470 <sharmavishal49313@gmail.com>
2023-01-11 17:54:43 +0530

    Adding files

---
+++ src/Banner.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Banner.css";
+
+function Banner(){
+    return(
+        <div className="mainComponent">
+            dfghnjvbn
+            
+        </div>
+    );
+}
+
+export default Banner;
---
+++ src/NewFile.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./NewFile.css";
+import Banner from "./Banner";
+
+function NewFile(){
+    return(
+        <div>
+            <h1>Hello</h1>
+            <p>Vishal Sharma</p>
+            <Banner />
+        </div>
+    );
+}
+
+export default NewFile;
